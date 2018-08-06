0d2d741d38ae3a005e5f4a986b44e1bc37cfa5cb
Eric Hopper <hopper@omnifarious.org>
2018-08-05 23:29:44 -0700

    A preliminary version sans bugs.

--- control.lua
+++ control.lua
@@ -1,21 +1,27 @@
+local function xy_add(p1, p2)
+   return {x = p1.x + p2.x,
+           y = p1.y + p2.y}
+end
+
 local function on_hotkey(event)
    local player = game.players[event.player_index]
    local speed = player.character_running_speed_modifier
    local msg = ""
 
-   if speed == 1 then
+   if speed == 0 then
       speed = 1.5
       msg = ">"
    elseif (speed > 1.49999) and (speed < 1.50001) then
       speed = 15
       msg = ">>"
    else
-      speed = 1
-      msg = "1"
+      speed = 0
+      msg = "0"
    end
+   player.character_running_speed_modifier = speed
    player.surface.create_entity(
       {name = "flying-text",
-       position = pos_offset(player.position,{x=-0.5,y=0.2}),
+       position = xy_add(player.position,{x=-0.5,y=0.2}),
        text = msg,
        color = {r=1, g=1, b=1}})
 end
