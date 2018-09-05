d41fadb3aed50c62375528c0eff6991a52fea206
Eric Hopper <hopper@omnifarious.org>
2018-09-04 22:17:47 -0700

    Added previously forgotten settings.lua - Oops!

---
+++ settings.lua
@@ -0,0 +1,23 @@
+_G.data:extend({
+	{
+		type = "double-setting",
+		name = "runspeeedtoggle-speed1",
+		setting_type = "runtime-per-user",
+                minimum_value = -0.99,
+		default_value = 0,
+	},
+	{
+		type = "double-setting",
+		name = "runspeeedtoggle-speed2",
+		setting_type = "runtime-per-user",
+                minimum_value = -0.99,
+		default_value = 1.5,
+	},
+	{
+		type = "double-setting",
+		name = "runspeeedtoggle-speed3",
+		setting_type = "runtime-per-user",
+                minimum_value = -0.99,
+		default_value = 15,
+	},
+})
